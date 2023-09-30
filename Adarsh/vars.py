b0441021279f0363afabe4ca410348523605668d
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2023-09-30 09:46:12 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,33 +9,32 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = '15914139'
-    API_HASH = 'f5c54e1dd806604552e211b3841c1ad4'
-    BOT_TOKEN = '5371686231:AAH-frIffQw2bAjW802b6Ilk7A6cXiuYqtA'
-    API = '8b088f1bec72e5db45502b832a1116b99e11e876'
-    SESSION_NAME = 'filetolinkbot'
-    SLEEP_THRESHOLD = '60'
-    WORKERS = '4'
-    BIN_CHANNEL = '-1001767960284'
-    PORT = 8080
-    BIND_ADRESS = '0.0.0.0'
-    PING_INTERVAL = '1200' # 20 minutes
-    OWNER_ID =  '1121140181'
-    NO_PORT = False
+    API_ID = int(getenv('API_ID', '15914139'))
+    API_HASH = str(getenv('API_HASH', 'f5c54e1dd806604552e211b3841c1ad4'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5371686231:AAH-frIffQw2bAjW802b6Ilk7A6cXiuYqtA'))
+    name = str(getenv('name', 'filetolinkbot'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001767960284'))
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1121140181").split())  
+    NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = "alpha"
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Alpha'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = "alpha"
+        APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = BIND_ADRESS
-    HAS_SSL= False
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = "mongodb+srv://enrich:enrich@cluster0.mhghl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    UPDATES_CHANNEL = None
-    BANNED_CHANNELS = None
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'unique_coders_x'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
